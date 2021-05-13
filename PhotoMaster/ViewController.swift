79dd8cf7a1982480ebef2d9e58fdb1cc41d48bb5
OtohaTanaka <otohaoppy@gmail.com>
2021-05-13 15:00:13 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by 田中音羽 on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
